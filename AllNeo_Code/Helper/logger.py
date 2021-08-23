db8a5ce7950da4953bb5e1a584110855419902a3
GhatakCode <anirban.ghatak@outlook.com>
2021-08-23 16:20:37 -0400

    Base Logger checkin.

---
+++ AllNeo_Code/Helper/logger.py
@@ -0,0 +1,24 @@
+import logging as logging
+import logging.config as logConfig 
+from logging.handlers import TimedRotatingFileHandler
+
+class NeoLogger:
+    #logConfig.fileConfig('./Global/Logging.conf')
+    
+    #create logger 
+    def getLogger(loggerName):
+        
+        allNeo_logger = logging.getLogger(loggerName)
+        allNeo_logger.setLevel(logging.DEBUG)
+        #following line is hardcoded and to be updated with config file setting.
+        allNeo_handler = TimedRotatingFileHandler(filename='Logs/AllNeoMasterLog.Log',backupCount= 30, when= 'D', interval= 1, delay= False, utc= True)
+        allNeo_format = logging.Formatter('%(asctime)s ::  %(name)s :: %(levelname)s -- %(message)s')
+        
+        #allNeo_handler.setLevel()        
+        allNeo_handler.setFormatter(allNeo_format)
+
+        allNeo_logger.addHandler(allNeo_handler)
+               
+        return allNeo_logger
+         
+    
